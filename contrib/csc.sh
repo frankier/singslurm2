637ac08d84ebda78fe32e1c5a02f987143f8b8a0
Frankie Robertson <frankie@robertson.name>
2021-02-23 22:45:03 +0200

    Fix up CSC contrib script

--- contrib/csc.sh
+++ contrib/csc.sh
@@ -2,10 +2,10 @@ PRE_SCRIPT=$(cat <<'END_PRE_SCRIPT'
 # From CSC singularity_wrapper
 CSC_SING_FLAGS="-B /users:/users -B /projappl:/projappl -B /scratch:/scratch -B /appl/data:/appl/data"
 if [ -n "$TMPDIR" ]; then
-    CSC_SING_FLAGS="$SING_FLAGS -B $TMPDIR:$TMPDIR"
+    CSC_SING_FLAGS="$CSC_SING_FLAGS -B $TMPDIR:$TMPDIR"
 fi
 if [ -n "$LOCAL_SCRATCH" ]; then
-    CSC_SING_FLAGS="$SING_FLAGS -B $LOCAL_SCRATCH:$LOCAL_SCRATCH"
+    CSC_SING_FLAGS="$CSC_SING_FLAGS -B $LOCAL_SCRATCH:$LOCAL_SCRATCH"
 fi
 SING_EXTRA_ARGS="$CSC_SING_FLAGS $SING_EXTRA_ARGS"
 END_PRE_SCRIPT
